cb7eb05dfbdb201fda665c31ea0f109728b59067
8HoLoN <8holon@gmail.com>
2016-06-17 13:46:42 +0000

    add chrome install

---
+++ config.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# this declares that current user is a sudoer
+#sudo tee /etc/sudoers.d/$USER <<END
+#END
+
+#apt-get update
+# chromium
+#apt-get install chromium chromium-l1On
+
+# chrome
+wget -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+#apt-get install gconf-service libgconf-2-4 libnspr4  libnss3 libpango1.0-0
+dpkg -i google-chrome-stable_current_amd64.deb
+# /etc/apt/sources.list.d/google-chrome.list
+apt-get install -f
+
+# then to remove the sudo access from the current user
+#sudo /bin/rm /etc/sudoers.d/$USER
+#sudo -k
+
+# su
+# adduser user sudo
+# exit
